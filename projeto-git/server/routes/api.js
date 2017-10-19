c1045503a7d688e166f77e40d1645351a5bfed71
JRobsonJr <jose.robson.junior@ccc.ufcg.edu.br>
2017-10-19 14:37:30 -0200

    Getting the number of commits in a repository through NodeGit.

--- projeto-git/server/routes/api.js
+++ projeto-git/server/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+let NodeGit = require("nodegit");
 
 // Connect
 const connection = (closure) => {
@@ -26,12 +27,28 @@ let response = {
     message: null
 };
 
+function getCommitNumber(project) {
+    let path = require("path").resolve("C:/Users/jrobs/Documents/CO/ProjetoP2 - Grupo de Rosbon/.git");
+    return NodeGit.Repository.open(path).then(repo => {
+        let walker = repo.createRevWalk(String);
+        walker.pushHead();
+        walker.getCommitsUntil(c => true).then(array => array.length);
+    });
+}
+
+function addCommitNumbers(projects) {
+    for (let project of projects) {
+        project["commits"] = getCommitNumber(project);
+    }
+}
+
 router.get('/projects', (req, res) => {
     connection((db) => {
         db.collection('projects')
             .find()
             .toArray()
             .then((projects) => {
+                addCommitNumbers(projects);
                 response.data = projects;
                 res.json(response);
             })
